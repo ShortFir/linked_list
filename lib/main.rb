690b033ff646f26273edde21ae692cf355f95c03
ShortFir <shortfir@protonmail.com>
2021-06-01 16:49:16 +1000

    Added newlines to display in main.rb.

--- lib/main.rb
+++ lib/main.rb
@@ -36,28 +36,29 @@ puts list
 print 'Size: ', list.size, "\n"
 list.prepend('pizza')
 puts list
-print 'Size: ', list.size, "\n"
+print 'Size: ', list.size, "\n\n"
 
 print 'Head: ', list.head, "\n"
-print 'Tail: ', list.tail, "\n"
+print 'Tail: ', list.tail, "\n\n"
 
 print 'Index -5: ', list.at(-5), "\n"
 print 'Index 0: ', list.at(0), "\n"
 print 'Index 2: ', list.at(2), "\n"
-print 'Index 10: ', list.at(10), "\n"
+print 'Index 10: ', list.at(10), "\n\n"
 
 puts list
 print 'Pop: ', list.pop, "\n"
 puts list
+puts
 
 print 'contains?(pizza): ', list.contains?('pizza'), "\n"
 print 'contains?(401): ', list.contains?(401), "\n"
 print 'contains?(alien): ', list.contains?('alien'), "\n"
-print 'contains?(4010): ', list.contains?(4010), "\n"
+print 'contains?(4010): ', list.contains?(4010), "\n\n"
 
 print 'find(401) - index: ', list.find(401), "\n"
 print 'find(wisdom) - index: ', list.find('wisdom'), "\n"
-print 'find(apples) - index: ', list.find('apples'), "\n"
+print 'find(apples) - index: ', list.find('apples'), "\n\n"
 
 print 'insert_at(house, 3): ', list.insert_at('house', 3), "\n"
 puts list
@@ -67,6 +68,7 @@ print 'insert_at(top, 0): ', list.insert_at('top', 0), "\n"
 puts list
 print 'insert_at(bottom, 10): ', list.insert_at('bottom', 10), "\n"
 puts list
+puts
 
 print 'remove_at(3): ', list.remove_at(3), "\n"
 puts list
